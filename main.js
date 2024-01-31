956e85a44a50328cd5fedcaf3cedf9410269249d
jonah <jonah@biorender.com>
2024-01-31 08:57:58 -0500

    Format and update comments

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 function draw(ctx) {
-	// clear the canvas
-	ctx.setTransform(1, 0, 0, 1, 0, 0);
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Clear the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-	// center the canvas
-	ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
+    // Center the canvas
+    ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
 
-	// styles
-	ctx.strokeStyle = '#333';
-	ctx.lineWidth = 2;
+    // Styles
+    ctx.strokeStyle = '#333';
+    ctx.lineWidth = 2;
 
-	drawDragon(ctx, 1);
+    drawDragon(ctx, 0);
+}
+
+// TODO
+function drawDragon(ctx, n) {
+    // Feel free to modify anything, adjust function signatures, add functions, etc. as you like :)
+    drawLine(ctx, -200, 0, 200, 0);
 }
 
 function drawLine(ctx, x1, y1, x2, y2) {
-	ctx.beginPath();
-	ctx.moveTo(x1, y1);
-	ctx.lineTo(x2, y2);
-	ctx.stroke();
-}
-
-function drawDragon(ctx, n, /* ... whatever else you would like :) */) {
-	// TODO
-	drawLine(ctx, -200, 0, 200, 0);
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
 }
 
 // Some biolerplate to draw on the fullscreen canvas
@@ -31,10 +32,10 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 function onResize() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-	draw(context);
+    draw(context);
 }
 
 window.addEventListener('load', onResize);
